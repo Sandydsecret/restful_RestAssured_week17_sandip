506c2d356ebf5424b5b5fc46c1a563dfed045cfa
sandydsecret <sandipghevariya35@gmail.com>
2023-05-31 10:08:29 +0100

    Homework RestAssured

---
+++ src/test/java/com/restful/booker/crudtest/BookingCRUDTest.java
@@ -0,0 +1,2 @@
+package com.restful.booker.crudtest;public class BookingCRUDTesr {
+}
---
+++ src/test/java/com/restful/booker/testsuite/AuthorisationPostTest.java
@@ -0,0 +1,6 @@
+package com.restful.booker.testsuite;
+
+import com.restful.booker.testbase.TestBase;
+
+public class AuthenticationPostTest extends TestBase {
+}
